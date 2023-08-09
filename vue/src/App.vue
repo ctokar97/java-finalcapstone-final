45f716e86e93970e3b0a5b71db3ffd26f69585a6
David Shimenko <davidshimenko@proton.me>
2023-08-08 23:37:12 -0400

    Update UI with Bootstrap and Custom Styles
    Upgraded the frontend UI by integrating Bootstrap and adding custom styles. More specifically, made the following changes:
    
    - Commented out previous viewport meta tag in 'vue/public/index.html' and added a new viewport meta tag which supports shrinking to fit. Also added the CDN link to incorporate Bootstrap stylesheet.
    
    - In addition, updated the styles in 'vue/src/App.vue' with customized animation and other styles for better UI experience.
    
    - Added the necessary Bootstrap packages to 'vue/package.json' and 'vue/package-lock.json' to ensure the seamless integration and functionality of Bootstrap components.
    
    - A minor change in 'vue/src/main.js', additional space added.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,3 +7,72 @@
     <router-view />
   </div>
 </template>
+
+<style>
+body {
+
+  height: 100vh;
+  width: 100vw;
+  overflow-x: hidden;
+  color: #fff;
+  background: linear-gradient(45deg, #52a6f1, #f3905a, #f3db59, #fbf2b7, #77ec86);
+  background-size: 400% 400%;
+  animation: gradientBG 15s ease infinite;
+}
+
+body::before {
+  position: absolute;
+  content: "";
+  left: 30%;
+  top: 20%;
+  width: 450px;
+  height: 450px;
+  background: #17141d;
+  border-radius: 62% 47% 82% 35% / 45% 45% 80% 66%;
+  will-change: border-radius, transform, opacity;
+  animation:sliderShape 5s linear infinite,
+  move 5s linear infinite;
+  display: block;
+  z-index: -1;
+  -webkit-animation: sliderShape 5s linear infinite, move 30s linear infinite;
+}
+
+@keyframes sliderShape{
+  0%,100%{
+    border-radius: 42% 58% 70% 30% / 45% 45% 55% 55%;
+    transform: translate3d(0,0,0) rotateZ(0.01deg);
+  }
+  34%{
+    border-radius: 70% 30% 46% 54% / 30% 29% 71% 70%;
+    transform:  translate3d(0,5px,0) rotateZ(0.01deg);
+  }
+  50%{
+    transform: translate3d(0,0,0) rotateZ(0.01deg);
+  }
+  67%{
+    border-radius: 100% 60% 60% 100% / 100% 100% 60% 60% ;
+    transform: translate3d(0,-3px,0) rotateZ(0.01deg);
+  }
+}
+
+@keyframes move {
+  0% { top: 10%; left: 10%;}
+  25% { top: 70%; left: 30%;}
+  50% { top: 50%; left: 70%;}
+  75% { top: 10%; left: 90%;}
+  100% { top: 80%; left: 10%;}
+}
+
+@keyframes gradientBG {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+</style>
